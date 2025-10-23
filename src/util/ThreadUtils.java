6d81591a7e6d09c4104c3d736f5b226b956cbd4c
sionPark <onioni0000002@gmail.com>
2025-10-23 19:59:45 +0900

    Add スレッドJoin

---
+++ src/util/ThreadUtils.java
@@ -0,0 +1,15 @@
+package util;
+
+import static util.MyLogger.log;
+
+public abstract class ThreadUtils  {
+
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            log("Interrupt 発生" + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
